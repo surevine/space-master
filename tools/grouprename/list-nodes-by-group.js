d61be834be6d17da4c8b2c27d177deac06f3b795
Administrator <admin@local.host>
2014-05-16 11:26:32 +0100

    Change made by RJ from a client of ours to prevent lucene results limits being hit as easily

--- tools/grouprename/list-nodes-by-group.js
+++ tools/grouprename/list-nodes-by-group.js
@@ -18,63 +18,53 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-*/
-function hasGroup(groups, targetGroup) {
-    if (groups) {
-        if (groups.indexOf(",") > -1) {
-            return groups.split(",").indexOf(targetGroup) > -1;
-        } else {
-            return groups == targetGroup;
-        }
-    }
-
-    return false;
-}
-
+ */
 function main() {
-    var result;
-    var skipNodes = args["skipNodes"];
-    var targetGroup = args["group"];
-    var targetType  = args["type"].toLowerCase();
+	var skipNodes = args["skipNodes"];
+	var targetGroup = args["group"];
+	var targetType = args["type"].toLowerCase();
 
-    logger.log("targetGroup " +targetGroup);
-    logger.log("targetType " +targetType);
+	logger.log("targetGroup " + targetGroup);
+	logger.log("targetType " + targetType);
 
-    var results = search.query({"query": "+ASPECT:\"{http://www.alfresco.org/model/enhancedSecurity/0.3}enhancedSecurityLabel\""});
+	var results;
 
-    logger.log("Found " +results.length +" items with a security label.");
+	if (targetType === "closed") {
+		logger.log("Searching for closed group: \@\\{http\\://www.alfresco.org/model/enhancedSecurity/0.3\\}closedMarkings:\""+targetGroup+"\""};
+		results = search.query({"query": "\@\\{http\\://www.alfresco.org/model/enhancedSecurity/0.3\\}closedMarkings:\""+targetGroup+"\""});
+	} else if (targetType === organisations{
+		logger.log("Searching for closed group: \@\\{http\\://www.alfresco.org/model/enhancedSecurity/0.3\\}organisations:\""+targetGroup+"\""};
+		results = search.query({"query": "\@\\{http\\://www.alfresco.org/model/enhancedSecurity/0.3\\}organisations:\""+targetGroup+"\""});
+	} else {
+		logger.log("Please specify a valid target type");
+		results = [];
+	}
 
-    var result = "";
-    var count = 0;
+	logger.log("Found " + results.length + " items with a security label.");
 
-    for (var i = 0; i<results.length; i++) {
-        if (skipNodes.indexOf(results[i].nodeRef) > -1) {
-            logger.warn("Skipping nodeRef: " +results[i].nodeRef);
-            continue;
-        }
+	var result = "";
+	var count = 0;
 
-        var organisations = "" + results[i].properties["{http://www.alfresco.org/model/enhancedSecurity/0.3}organisations"];
-        var closedMarkings = "" + results[i].properties["{http://www.alfresco.org/model/enhancedSecurity/0.3}closedMarkings"];
+	for (var i = 0; i < results.length; i++) {
+		if (skipNodes.indexOf(results[i].nodeRef) > -1 {
+			logger.warn("Skipping nodeRef: " + results[i].nodeRef);
+			continue;
+		}
 
-        try {
-            results[i].displayPath;
-        } catch (e) {
-            logger.warn("Node " +results[i].nodeRef +" failed with: " +e);
-            continue;
-        }
+		try {
+			results[i].displayPath;
+		} catch (e) {
+			logger.warn("Node " + results[i].nodeRef + " failed with: " + e);
+			continue;
+		}
 
-        if (targetType === "organisations" && hasGroup(organisations, targetGroup)) {
-            result += results[i].nodeRef +"|" +results[i].displayPath +"/" +results[i].name +"\n";
-            count++;
-        } else if (targetType === "closed" && hasGroup(closedMarkings, targetGroup)) {
-            result += results[i].nodeRef +"|" +results[i].displayPath +"/" +results[i].name +"\n";
-            count++;
-        }
-    }
+		result += results[i].nodeRef + "|" + results[i].displayPath + "/" + results[i].name + "\n";
+		count++;
+	}
 
-    logger.log(count +" items have the group " +targetGroup +".");
+	logger.log(count + " items have the group " + targetGroup + ".");
 
-    return result;
+	return result;
 }
 
 main();
