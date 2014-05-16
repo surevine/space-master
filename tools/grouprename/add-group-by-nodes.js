2ce2fafd5a111a76771853b53067178ab39e39b1
Administrator <admin@local.host>
2014-05-16 13:59:37 +0100

    Extra logging and better handling of collections

--- tools/grouprename/add-group-by-nodes.js
+++ tools/grouprename/add-group-by-nodes.js
@@ -22,47 +22,69 @@
 var count = 0;
 
 function addGroup(collection, group) {
-    if (!collection) collection = "";
+    if (!collection) collection = [];
 
-    if (collection.indexOf(",") > -1) {
-        var items = collection.split(",");
+    logger.log("AddGroup:        Adding Group "+group+" to collection "+collection+" with "+collection.length+" elements");
+    
+    if (collection.length > 0) {
+    	logger.log("AddGroup:        Collection has pre-existing groups")
 
-        if (items.indexOf(group) > -1) {
-            return {"hasChanges": false};
+        if (collection.join(" ").indexOf(group) > -1) {
+            return {"hasChanges": false, "content": collection};
         }
-
-        items.push(group);
+    	collection.push(group);
         count++;
-        return {"hasChanges": true, "content": items};
+        return {"hasChanges": true, "content": collection};
     } else {
-        if (collection == group) {
-            return {"hasChanges": false};
-        } else {
-            count++;
-            return {"hasChanges": true, "content": [group]};
-        }
+    	logger.log("AddGroup:        Collection has no pre-existing groups")
+        count++;
+    	collection=[];
+    	collection.push(group);
+        return {"hasChanges": true, "content": collection};
     }
+}
+
+// Need to break this out as is Java array under the covers and so doesn't .toString nicely
+function renderGroups(groups) {
+	try {
+		var rV="[";
+		for (var i=0; i < groups.length; i++) {
+			rV=rV+groups[i]+" ";
+		}
+		return rV+"]";
+	}
+	catch (e) {
+		return "Could not render groups list: "+e;
+	}
 }
 
 function main() {
     var result = "";
 
-    logger.log("Running add group by nodeRefs");
+    logger.log("AddGroup:  Running add group by nodeRefs");
 
     var targetGroup = args["group"];
     var targetNodes = args["nodeRefs"];
     var targetType = args["type"].toLowerCase();
 
     var nodeRefs = targetNodes.split("\n");
-    for (var i = 0; i<nodeRefs.length; i++) {
-        var nodeRef = nodeRefs[i].substring(0, nodeRefs[i].indexOf('|'));
-        var path = nodeRefs[i].substring(nodeRefs[i].indexOf('|')+1);
+    
+    logger.log("AddGroup:  Parameters supplied - targetGroup = "+targetGroup+" & targetType = "+targetType+" & nodeRefs = ("+targetNodes.length+" nodes)");
 
+    for (var i = 0; i<nodeRefs.length; i++) {
+    	logger.log("AddGroup:  Processing node with index "+i);
+    	
+    	var saveChanges=false;
+        var nodeRef = nodeRefs[i].substring(0, nodeRefs[i].indexOf('|'));
+        logger.log("AddGroup:    NodeRef = "+nodeRef);
+        var path = nodeRefs[i].substring(nodeRefs[i].indexOf('|')+1);
+        logger.log("AddGroup:    Path = "+path);
         var node;
         try {
             node = search.findNode(nodeRef);
+            logger.log("AddGroup:    Succesfully found the node "+nodeRef+" = "+node);
         } catch (e) {
-            logger.warn("Failed to findNode(" +nodeRef +"). Perhaps it doesn't exist. The expected path was:\n" +path);
+            logger.warn("AddGroup:    Failed to findNode(" +nodeRef +"). Perhaps it doesn't exist. The expected path was:" +path);
             continue;
         }
 
@@ -70,42 +92,51 @@ function main() {
             var organisations = node.properties["{http://www.alfresco.org/model/enhancedSecurity/0.3}organisations"];
             var groups = addGroup(organisations, targetGroup);
             if (groups.hasChanges) {
+            	saveChanges=true;
                 logger.log("Setting organisations to " +groups.content);
                 node.properties["{http://www.alfresco.org/model/enhancedSecurity/0.3}organisations"] = groups.content;
             }
         } else if (targetType === "closed") {
             var closedMarkings = node.properties["{http://www.alfresco.org/model/enhancedSecurity/0.3}closedMarkings"];
+            logger.log("AddGroup:    Existing closed markings = "+renderGroups(closedMarkings));
             var groups = addGroup(closedMarkings, targetGroup);
+            logger.log("AddGroup:    New closed markings = "+renderGroups(groups.content));
             if (groups.hasChanges) {
-                logger.log("Setting closedMarkings to " +groups.content);
+            	saveChanges=true;
+                logger.log("AddGroup:    Groups have changed so setting closedMarkings to " +groups.content);
                 node.properties["{http://www.alfresco.org/model/enhancedSecurity/0.3}closedMarkings"] = groups.content;
+                logger.log("AddGroup:    Markings set but not saved");
+            }
+            else {
+                logger.log("AddGroup:    Groups have not changed so no changes required");
             }
         } else if (targetType === "open") {
             var closedMarkings = node.properties["{http://www.alfresco.org/model/enhancedSecurity/0.3}openMarkings"];
             var groups = addGroup(openMarkings, targetGroup);
             if (groups.hasChanges) {
+            	saveChanges=true;
                 logger.log("Setting openMarkings to " +groups.content);
                 node.properties["{http://www.alfresco.org/model/enhancedSecurity/0.3}openMarkings"] = groups.content;
             }
         }
 
-        if (count > 0) {
+        if (saveChanges) {
             try {
                 node.save();
-                logger.log("Successfully updated " +nodeRef +" with path:\n" +path);
+                logger.log("AddGroup:    Successfully updated " +nodeRef +" with path:\n" +path);
             } catch (e) {
-                logger.log("Failed to update " +nodeRef +" with path:\n" +path +" due to: " +e);
+                logger.log("AddGroup:    Failed to update " +nodeRef +" with path:\n" +path +" due to: " +e);
                 count--;
             }
         } else {
-            logger.log("Group " +targetGroup +" already on " +nodeRef +" with path:\n" +path);
+            logger.log("AddGroup:    Group " +targetGroup +" already on " +nodeRef +" with path:\n" +path);
         }
     }
 
     if (count > 0) {
-        logger.log("Added " +targetGroup +" to " +count +" nodes.");
+        logger.log("AddGroup:  Added " +targetGroup +" to " +count +" nodes.");
     } else {
-        logger.log("No changes made.");
+        logger.log("AddGroup:  No changes made.");
     }
 
     return result;
